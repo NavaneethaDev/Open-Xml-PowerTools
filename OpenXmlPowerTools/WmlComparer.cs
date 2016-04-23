77af69c645dd6c1a9a2d33697e5899bf8d39d520
EricWhiteDev <eric@ericwhite.com>
2016-04-23 13:35:19 +0530

    WIP

--- OpenXmlPowerTools/WmlComparer.cs
+++ OpenXmlPowerTools/WmlComparer.cs
@@ -129,6 +129,13 @@ namespace OpenXmlPowerTools
                             d.Name == W.bookmarkStart ||
                             d.Name == W.bookmarkEnd)
                 .Remove();
+            wDoc.MainDocumentPart
+                .GetXDocument()
+                .Root
+                .Descendants()
+                .Attributes()
+                .Where(a => a.Name.Namespace == PtOpenXml.pt)
+                .Remove();
             wDoc.MainDocumentPart.PutXDocument();
         }
 
@@ -145,7 +152,8 @@ namespace OpenXmlPowerTools
                 var s = cloneParaForHashing.ToString(SaveOptions.DisableFormatting)
                     .Replace(" xmlns=\"http://schemas.openxmlformats.org/wordprocessingml/2006/main\"", "");
                 var ancestorsString = para.Ancestors().TakeWhile(a => a.Name != W.body).Select(a => a.Name.LocalName + "/").StringConcatenate();
-                var sha1Hash = SHA1HashStringForUTF8String(s + ancestorsString);
+                var shaString = s + ancestorsString;
+                var sha1Hash = SHA1HashStringForUTF8String(shaString);
                 var pPr = para.Element(W.pPr);
                 if (pPr == null)
                 {
@@ -253,9 +261,9 @@ namespace OpenXmlPowerTools
                     return newRuns;
                 }
 
-                if (element.Name == A.blip)
+                if (ComparisonUnit.s_ElementsWithRelationshipIds.Contains(element.Name))
                 {
-                    var newBlip = new XElement(element.Name,
+                    var newElement = new XElement(element.Name,
                         element.Attributes().Select(a =>
                         {
                             if (!ComparisonUnit.s_RelationshipAttributeNames.Contains(a.Name))
@@ -264,18 +272,22 @@ namespace OpenXmlPowerTools
                             OpenXmlPart oxp = mainDocumentPart.GetPartById(rId);
                             if (oxp == null)
                                 throw new FileFormatException("Invalid WordprocessingML Document");
-                            byte[] buffer = new byte[1024];
-                            using (var str = oxp.GetStream())
+                            if (!oxp.ContentType.EndsWith("xml"))
                             {
-                                var ret = str.Read(buffer, 0, buffer.Length);
-                                if (ret == 0)
-                                    throw new FileFormatException("Image contains no data");
+                                byte[] buffer = new byte[1024];
+                                using (var str = oxp.GetStream())
+                                {
+                                    var ret = str.Read(buffer, 0, buffer.Length);
+                                    if (ret == 0)
+                                        throw new FileFormatException("Image contains no data");
+                                }
+                                var b64string = Convert.ToBase64String(buffer);
+                                return new XAttribute(a.Name, b64string);
                             }
-                            var b64string = Convert.ToBase64String(buffer);
-                            return new XAttribute(a.Name, b64string);
+                            return null;
                         }),
                         element.Nodes().Select(n => CloneParaForHashing(mainDocumentPart, n)));
-                    return newBlip;
+                    return newElement;
                 }
 
                 if (element.Name == VML.shape)
@@ -1755,6 +1767,39 @@ namespace OpenXmlPowerTools
         {
             Contents = contentAtomList.ToList();
         }
+
+        public static XName[] s_ElementsWithRelationshipIds = new XName[] {
+            A.blip,
+            A.hlinkClick,
+            A.relIds,
+            C.chart,
+            C.externalData,
+            C.userShapes,
+            DGM.relIds,
+            O.OLEObject,
+            VML.fill,
+            VML.imagedata,
+            VML.stroke,
+            W.altChunk,
+            W.attachedTemplate,
+            W.control,
+            W.dataSource,
+            W.embedBold,
+            W.embedBoldItalic,
+            W.embedItalic,
+            W.embedRegular,
+            W.footerReference,
+            W.headerReference,
+            W.headerSource,
+            W.hyperlink,
+            W.printerSettings,
+            W.recipientData,
+            W.saveThroughXslt,
+            W.sourceFileName,
+            W.src,
+            W.subDoc,
+            WNE.toolbarData,
+        };
 
         public static XName[] s_RelationshipAttributeNames = new XName[] {
             R.embed,
